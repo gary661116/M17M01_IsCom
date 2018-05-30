f8f431ba36a8920f7c248f652f8c1253dbba6688
Gary Chang <gary661116@gmail.com>
2018-05-31 00:47:14 +0800

    產品 Menu變更，經典案例

---
+++ OutWeb/Controllers/ClassicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutWeb.Controllers
+{
+    public class ClassicController : Controller
+    {
+        // GET: Classic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
